ce6ddc620292f305e3514202f9c63a7dca88236c
Guilherme Braga <89932943+guilhermevnbraga@users.noreply.github.com>
2024-05-22 18:31:22 -0300

    a

--- italk/app/api/auth/[...nextauth]/route.ts
+++ italk/app/api/auth/[...nextauth]/route.ts
@@ -27,7 +27,7 @@ const handler = NextAuth({
             async authorize(credentials) {
                 try {
                     const response = await fetch(
-                        "http://localhost:3001/login",
+                        "https://italk-server-iq2u9t70q-s0la1r3s-projects.vercel.app/login",
                         {
                             method: "POST",
                             headers: {
