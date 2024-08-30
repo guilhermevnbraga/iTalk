d4391e431e5db119f8a338269c70ea5b85a20ec1
Guilherme Braga <89932943+guilhermevnbraga@users.noreply.github.com>
2024-08-30 17:55:29 -0300

    cros...

--- italkserver/index.js
+++ italkserver/index.js
@@ -1,6 +1,5 @@
 const { PrismaClient } = require("@prisma/client");
 const bcrypt = require("bcrypt");
-const cors = require("cors");
 const express = require("express");
 const multer = require("multer");
 const { v4: uuidv4 } = require("uuid");
